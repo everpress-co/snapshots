c9ed44abc66f883c4ffa3b9f16bcd7cd291cee59
Xaver <881729+evrpress@users.noreply.github.com>
2023-03-13 19:41:28 +0100

    version bump

--- snapshots.php
+++ snapshots.php
@@ -6,7 +6,7 @@ Description:     Quickly Create SnapShots of your development sites and restore 
 Author:          EverPress
 Author URI:      https://about.me/xaver
 Text Domain:     snapshots
-Version:         2.6.0
+Version:         2.7.0
  */
 
 if ( version_compare( PHP_VERSION, '7.0' ) < 0 ) {
